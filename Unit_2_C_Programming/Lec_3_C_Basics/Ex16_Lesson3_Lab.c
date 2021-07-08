ea7e6d7f92ea2dba06727098e5e0f73d90f6b80a
Arsany <arsanyashrafmounir@gmail.com>
2021-07-08 15:10:11 +0200

    End of Lec3 Assigments

---
+++ Unit_2_C_Programming/Lec_3_C_Basics/Ex16_Lesson3_Lab.c
@@ -0,0 +1,20 @@
+/*
+ * Ex16_Lesson3_Lab.c
+ *
+ *  Created on: Jul 8, 2021
+ *      Author: Arsany
+ */
+#include "stdio.h"
+void main()
+{
+	int i,j;
+	for(i=0;i<10;i++)
+	{
+		for(j=i;j<10;j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+}
+
