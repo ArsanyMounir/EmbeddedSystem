5ec0815a4e2545ccac0fe497d07322bb6aec277e
Arsany <Arsanyashrafmounir@gmail.com>
2024-01-02 19:22:30 +0200

    Ex1 update

--- Unit_2_C_Programming/Lec_4_Array_String/Ex1_Multidimention_Array.c
+++ Unit_2_C_Programming/Lec_4_Array_String/Ex1_Multidimention_Array.c
@@ -1,51 +1,56 @@
 /*
- * Ex1_Multidimention_Array.c
+ * main.c
  *
- *  Created on: Jul 11, 2021
+ *  Created on: Jan 2, 2024
  *      Author: Arsany
  */
 #include "stdio.h"
-void main()
+
+int main ()
 {
-	float m1[2][2],m2[2][2],sumMatrix[2][2];
+	float m1[2][2],m2[2][2],sum[2][2];
 	int i,j;
 	printf("Enter the elements of 1st matrix \n");
-	for(i=0;i<2;i++)
+	fflush(stdin);fflush(stdout);
+	for (i=0;i<2;i++)
 	{
 		for(j=0;j<2;j++)
 		{
-			printf("Enter a%d%d:",i+1,j+1);
-			fflush(stdin);
-			fflush(stdout);
+			printf("Enter a%d%d: ",i+1,j+1);
+			fflush(stdin);fflush(stdout);
 			scanf("%f",&m1[i][j]);
+			fflush(stdin);fflush(stdout);
 		}
 	}
 	printf("Enter the elements of 2nd matrix \n");
-	for(i=0;i<2;i++)
+	fflush(stdin);fflush(stdout);
+	for (i=0;i<2;i++)
 	{
 		for(j=0;j<2;j++)
 		{
-			printf("Enter b%d%d:",i+1,j+1);
-			fflush(stdin);
-			fflush(stdout);
+			printf("Enter b%d%d: ",i+1,j+1);
+			fflush(stdin);fflush(stdout);
 			scanf("%f",&m2[i][j]);
 		}
 	}
-	for(i=0;i<2;i++)
+	for (i=0;i<2;i++)
 	{
 		for(j=0;j<2;j++)
 		{
-			sumMatrix[i][j]=m1[i][j]+m2[i][j];
+			sum[i][j]=m1[i][j]+m2[i][j];
 		}
 	}
-	printf("Sum Of Matrix:\n");
-	for(i=0;i<2;i++)
+	printf("Sum of Matrix:\n");
+	for (i=0;i<2;i++)
 	{
 		for(j=0;j<2;j++)
 		{
-			printf("%.2f\t",sumMatrix[i][j]);
+			printf("%.2f  ",sum[i][j]);
 		}
 		printf("\n");
 	}
 
+
+
+	return 0;
 }
