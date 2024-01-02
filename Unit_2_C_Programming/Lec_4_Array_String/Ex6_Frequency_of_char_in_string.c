02bdeefb06dfa9c94628c09c2f9e2c32a6aa3278
Arsany <Arsanyashrafmounir@gmail.com>
2024-01-02 19:24:12 +0200

    Ex6 update

--- Unit_2_C_Programming/Lec_4_Array_String/Ex6_Frequency_of_char_in_string.c
+++ Unit_2_C_Programming/Lec_4_Array_String/Ex6_Frequency_of_char_in_string.c
@@ -1,31 +1,27 @@
 /*
- * Ex6_Frequency_of_char_in_string.c
+ * main.c
  *
- *  Created on: Jul 11, 2021
+ *  Created on: Jan 2, 2024
  *      Author: Arsany
  */
-#include"stdio.h"
-#include"string.h"
-void main()
+#include "stdio.h"
+#include "string.h"
+int main ()
 {
-	char string[100],c;
-	int i,counter=0;
-	printf("Enter a string: ");
-	fflush(stdin);
-	fflush(stdout);
+	char string[100],character;
+	int i,counter=0,length;
+	printf("Enter a string :");
+	fflush(stdin);fflush(stdout);
 	gets(string);
-	printf("Enter a character to find frequency: ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%c",&c);
-	//printf("%d",strlen(string));
-	for(i=0;i<strlen(string);i++)
+	printf("Enter a charcter to find frequency :");
+	fflush(stdin);fflush(stdout);
+	scanf("%c",&character);
+	length = strlen(string);
+	for(i=0;i<length;i++)
 	{
-		if(c==string[i])
-		{
+		if (character == string[i])
 			counter++;
-		}
-
 	}
-	printf("Frequency of %c = %d",c,counter);
+	printf("Frequency of %c = %d ",character , counter);
+	return 0;
 }
