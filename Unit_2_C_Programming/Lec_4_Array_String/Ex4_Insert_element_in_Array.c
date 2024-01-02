ea89b16a25db8f707e7a9042bd73eb029ff71d6f
Arsany <Arsanyashrafmounir@gmail.com>
2024-01-02 19:23:38 +0200

    Ex4 update

--- Unit_2_C_Programming/Lec_4_Array_String/Ex4_Insert_element_in_Array.c
+++ Unit_2_C_Programming/Lec_4_Array_String/Ex4_Insert_element_in_Array.c
@@ -1,38 +1,46 @@
 /*
- * Ex4_Insert_element_in_Array.c
+ * main.c
  *
- *  Created on: Jul 11, 2021
+ *  Created on: Jan 2, 2024
  *      Author: Arsany
  */
-#include"stdio.h"
-void main()
-{
-	int i,arr[20],number,location,element;
-	printf("Enter no of elements : ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%d",&number);
-	for(i=0;i<number;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-	printf("Enter the element to be inserted : ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%d",&element);
-	printf("\nEnter the location : ");
-	fflush(stdin);
-	fflush(stdout);
-	scanf("%d",&location);
-	for(i=number;i>=location;i--)
-	{
-		arr[i]=arr[i-1];
-	}
-	number++;
-	arr[location-1]=number;
-	for(i=0;i<number;i++)
-	{
-		printf("%d ",arr[i]);
-	}
-}
+#include "stdio.h"
 
+int main ()
+{
+	float m[100],element;
+	int i,j,x;
+	printf("Enter no of elements :");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&x);
+	while(x>100||x<0)
+	{
+		printf("invalid input , range should be between 1 and 100 \n");
+		printf("Please re-enter no of elements");
+		fflush(stdin);fflush(stdout);
+		scanf("%d",&x);
+	}
+	printf("Enter elements of array :\n");
+	for (i=0;i<x;i++)
+	{
+		printf("Enter element %d : ",i+1);
+		fflush(stdin);fflush(stdout);
+		scanf("%f",&m[i]);
+	}
+	printf("Enter the element to be inserted :");
+	fflush(stdin);fflush(stdout);
+	scanf("%f",&element);
+	printf("Enter the location :");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&j);
+	for(i=x;i>=j;i--)
+	{
+		m[i]=m[i-1];
+	}
+	m[j-1]=element;
+	for(i=0;i<=x;i++)
+	{
+		printf("%.2f ",m[i]);
+	}
+	return 0;
+}
