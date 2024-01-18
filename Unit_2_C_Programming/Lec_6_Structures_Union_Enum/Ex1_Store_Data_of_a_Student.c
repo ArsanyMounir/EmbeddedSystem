317382a539e6d3ac1696aa0bb1635101a075204f
Arsany <Arsanyashrafmounir@gmail.com>
2024-01-18 16:51:21 +0200

    Ex1 Update

--- Unit_2_C_Programming/Lec_6_Structures_Union_Enum/Ex1_Store_Data_of_a_Student.c
+++ Unit_2_C_Programming/Lec_6_Structures_Union_Enum/Ex1_Store_Data_of_a_Student.c
@@ -1,33 +1,52 @@
 /*
- * Ex1_Student_data_entry.c
+ * main.c
  *
- *  Created on: Jul 19, 2021
+ *  Created on: Jan 16, 2024
  *      Author: Arsany
  */
 #include "stdio.h"
 
-struct Sstudent{
+struct Sstudent
+{
 	char name[50];
 	int roll;
-	float mark;
+	float marks;
 };
-void main()
+struct Sstudent readData();
+void printData(struct Sstudent data);
+int main()
 {
-	struct Sstudent s;
-	printf("Enter information of students:\n");
-	printf("Enter name:");
-	fflush(stdin);fflush(stdout);
-	gets(s.name);
-	printf("Enter roll number:");
-	fflush(stdin);fflush(stdout);
-	scanf("%d",&s.roll);
-	printf("Enter marks:");
-	fflush(stdin);fflush(stdout);
-	scanf("%f",&s.mark);
-	printf("\nDisplaying Information\n");
-	printf("Name: %s\n",s.name);
-	printf("Roll no.: %d\n",s.roll);
-	printf("Marks: %.3f",s.mark);
+	struct Sstudent dataBase[10];
+	int i=0,flag=1;
+	while (flag == 1)
+	{
+		dataBase[i]=readData();
+		printData(dataBase[i]);
+		printf("Enter 1 to enter another student , 0 to exit");
+		fflush(stdin);fflush(stdout);
+		scanf("%d",&flag);
+	}
+	return 0;
 
+}
+struct Sstudent readData()
+{
+	struct Sstudent data;
+	printf("Enter information of students:\n");
+	printf("Enter name :");
+	fflush(stdin);fflush(stdout);
+	gets(data.name);
+	printf("Enter roll number :");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&data.roll);
+	printf("Enter Marks :");
+	fflush(stdin);fflush(stdout);
+	scanf("%f",&data.marks);
+	return data;
+}
+void printData(struct Sstudent data)
+{
+	fflush(stdin);fflush(stdout);
+	printf("Displaying input data\n\nname : %s \nRoll: %d \nMarks: %.3f\n",data.name,data.roll,data.marks);
 }
 
