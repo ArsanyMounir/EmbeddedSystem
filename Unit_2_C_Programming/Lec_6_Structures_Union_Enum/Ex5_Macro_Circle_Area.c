3eec1287f5b595ed9949b43ef39f5d1f11af2885
Arsany <Arsanyashrafmounir@gmail.com>
2024-01-18 16:52:14 +0200

    Ex5 Update

--- Unit_2_C_Programming/Lec_6_Structures_Union_Enum/Ex5_Macro_Circle_Area.c
+++ Unit_2_C_Programming/Lec_6_Structures_Union_Enum/Ex5_Macro_Circle_Area.c
@@ -1,22 +1,21 @@
 /*
- * Ex5_Macro_Circle_Area.c
+ * main.c
  *
- *  Created on: Jul 19, 2021
+ *  Created on: Jan 16, 2024
  *      Author: Arsany
  */
 #include "stdio.h"
-#define pi 3.1415
-#define area(x)(pi*(x)*(x))
+#define pi 3.14
+#define areaCalc(x)(pi*(x)*(x))
+
 
 int main()
 {
-	int r;
-	float a;
-	printf("Enter the radius: ");
+	float  radius,area;
+	printf("Enter radius :");
 	fflush(stdin);fflush(stdout);
-	scanf("%d",&r);
-	a=area(r);
-	printf("Area = %.3f",a);
+	scanf("%f",&radius);
+	area=areaCalc(radius);
+	printf("Area = %.2f",area);
 	return 0;
 }
-
