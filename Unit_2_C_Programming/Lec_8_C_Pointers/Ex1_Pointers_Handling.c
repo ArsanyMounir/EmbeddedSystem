2f4091f5d9ade4cc0c3672ba9b316d3ad490449e
Arsany <Arsanyashrafmounir@gmail.com>
2024-01-24 16:20:26 +0200

    Ex1 Update

--- Unit_2_C_Programming/Lec_8_C_Pointers/Ex1_Pointers_Handling.c
+++ Unit_2_C_Programming/Lec_8_C_Pointers/Ex1_Pointers_Handling.c
@@ -1,30 +1,29 @@
 /*
- * Ex1_Pointers_Handling.c
+ * main.c
  *
- *  Created on: Jul 23, 2021
+ *  Created on: Jan 19, 2024
  *      Author: Arsany
  */
-#include "stdio.h"
 
-int main()
+#include"stdio.h"
+
+int main ()
 {
-	int m=29;
-	int* ab;
-	printf("Address of m : 0x%p\n",&m);
-	printf("Value of m : %d\n\n",m);
+	int m,*ab;
+	m=29;
+	printf("Address of m : 0x%x \n",&m);
+	printf("Value of m : %d\n",m);
 	ab=&m;
-	printf("Now ab is assigned with the address of m.\n");
-	printf("Address of pointer ab : 0x%p\n",ab);
-	printf("Value of pointer ab : %d\n\n",*ab);
-	m=34;
-	printf("TThe value of m assigned to %d now.\n",m);
-	printf("Address of pointer ab : 0x%p\n",ab);
-	printf("Value of pointer ab : %d\n\n",*ab);
-	*ab=7;
-	printf("The pointer variable ab is assigned with the value %d now.\n",*ab);
-	printf("Address of m : 0x%p\n",&m);
-	printf("Value of m : %d",m);
+	printf("Address of pointer ab : 0x%x \n",ab);
+	printf("Content of pointer ab : %d\n",*ab);
+	m = 34;
+	printf("Address of pointer ab : 0x%x \n",ab);
+	printf("Content of pointer ab : %d\n",*ab);
+	*ab = 7;
+	printf("Address of m : 0x%x \n",&m);
+	printf("Value of m : %d\n",m);
+
+
+
 	return 0;
-
 }
-
