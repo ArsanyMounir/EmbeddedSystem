c0e050d2debd6e296b05e324374e9a8685d1e89e
Arsany <Arsanyashrafmounir@gmail.com>
2024-01-24 16:21:30 +0200

    Ex5 Update , End of lec8 Assignment

--- Unit_2_C_Programming/Lec_8_C_Pointers/Ex5_pointer_to_array_of_pointer_to_structure.c
+++ Unit_2_C_Programming/Lec_8_C_Pointers/Ex5_pointer_to_array_of_pointer_to_structure.c
@@ -1,23 +1,30 @@
 /*
- * Ex5_pointer_to_array_of_pointer_to_structure.c
+ * main.c
  *
- *  Created on: Jul 23, 2021
+ *  Created on: Jan 19, 2024
  *      Author: Arsany
  */
+
 #include"stdio.h"
 
-struct Semployee{
-	char name[50];
-	int id;
-};
-int main()
-{
-	struct Semployee emp0={"Alex",1002};
-	struct Semployee *Ptr_emp[10];
-	struct Semployee *(*PTR)[10]=Ptr_emp;
-	Ptr_emp[0]=&emp0;
-	printf("ExmployeeName : %s\n",(*(*PTR))->name);
-	printf("Employee ID : %d",(*(*PTR))->id);
 
+struct SEmployee{
+	char name[20];
+	int ID;
+};
+
+
+int main ()
+{
+	struct SEmployee emp0={"Ahmed",1001};
+	struct SEmployee emp1={"Alex",1002};
+	struct SEmployee emp2={"Arsany",1003};
+	struct SEmployee *ptr_Struct[10];
+	ptr_Struct[0]=&emp0;
+	ptr_Struct[1]=&emp1;
+	ptr_Struct[2]=&emp2;
+	struct SEmployee *(*Ptr)[10]=ptr_Struct;
+	printf("Employer name :%s\n",(*(*Ptr)[1]).name);
+	printf("Employer ID :%d",(*(*Ptr)[1]).ID);
 	return 0;
 }
