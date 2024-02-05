9e783b80aae8e8773a20780fc041d82ab50eab06
Arsany <Arsanyashrafmounir@gmail.com>
2024-02-05 03:55:08 +0200

    updating Led Toggle lab

--- Unit_3_Embedded_C/Lec_1/STM32F103C6_Led_Toggle/main.c
+++ Unit_3_Embedded_C/Lec_1/STM32F103C6_Led_Toggle/main.c
@@ -6,7 +6,7 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * <h2><center>&copy; Copyright (c) 2024 STMicroelectronics.
  * All rights reserved.</center></h2>
  *
  * This software component is licensed by ST under BSD 3-Clause license,
@@ -16,25 +16,44 @@
  *
  ******************************************************************************
  */
+
 #include "stdint.h"
-//Registers_Definations
-#define RCC_BASE        0x40021000
-#define PORTA_BASE      0x40010800
-#define RCC_APB2ENR     *(volatile uint32_t *) (RCC_BASE +0x18)
-#define GPIOA_CRH       *(volatile uint32_t *) (PORTA_BASE +0x04)
-#define GPIOA_ODR       *(volatile uint32_t *) (PORTA_BASE +0x0C)
+typedef volatile unsigned int vuint32_t ;
+
+#define RCC_BASE					0x40021000
+#define GPIO_PORTA_BASE				0x40010800
+#define RCC_APB2ENR					*(vuint32_t *) (RCC_BASE + 0x18)
+
+typedef union {
+		vuint32_t all_fields;
+		struct{
+		vuint32_t reserved:13;
+		vuint32_t P_13:1;
+		}Pin;
+}R_ODR_t;
+
+typedef union {
+		vuint32_t all_fields;
+		struct{
+		vuint32_t reserved:20;
+		vuint32_t P_13:4;
+		}Pin;
+}R_CRH_t;
+
+volatile R_ODR_t * R_ODR =  (volatile R_ODR_t * )(GPIO_PORTA_BASE + 0x0C) ;
+volatile R_CRH_t * R_CRH =  (volatile R_CRH_t * )(GPIO_PORTA_BASE + 0x04) ;
 
 int main(void)
 {
-
-	RCC_APB2ENR |= 1<<2;
-	GPIOA_CRH &= 0xff0fffff;
-	GPIOA_CRH |= 0x00200000;
+	volatile int i;
+	RCC_APB2ENR |= (1<<2);
+	R_CRH->all_fields &= 0xFF0FFFFF;
+	R_CRH->Pin.P_13 |= 0x02;
 	while(1)
 	{
-		GPIOA_ODR |=1<<13;
-		for(int i=0;i<50000;i++);
-		GPIOA_ODR &= ~(1<<13);
-		for(int i=0;i<50000;i++);
+		R_ODR->Pin.P_13 = 1;
+		for(i=0;i<50000;i++);
+		R_ODR->Pin.P_13 = 0;
+		for(i=0;i<50000;i++);
 	}
 }
