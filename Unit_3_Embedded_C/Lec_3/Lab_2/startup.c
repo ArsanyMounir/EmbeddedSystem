cad70adc399c965fdce4c820f0563430d458d0fd
Arsany <Arsanyashrafmounir@gmail.com>
2024-02-07 21:21:42 +0200

    Updating lab_2 makefile,building process from scratch

--- Unit_3_Embedded_C/Lec_3/Lab_2/startup.c
+++ Unit_3_Embedded_C/Lec_3/Lab_2/startup.c
@@ -1,53 +1,57 @@
-//startup.c
-//End.Arsany
-#include"stdint.h"
+#include "stdint.h"
 extern int main();
+extern uint32_t _stack_top;
 void Reset_Handler();
+
 void Default_Handler()
 {
 	Reset_Handler();
 }
-void NMI_Handler() __attribute__((weak,alias("Default_Handler")));
-void H_Fault_Handler()__attribute__((weak,alias("Default_Handler")));
-void MM_Fault_Handler()__attribute__((weak,alias("Default_Handler")));
-void Bus_Fault_Handler()__attribute__((weak,alias("Default_Handler")));
-void Usage_Fault_Handler()__attribute__((weak,alias("Default_Handler")));
 
-extern unsigned int _stack_top;
+void NMI_Handler() __attribute__((weak ,alias("Default_Handler")));
+void H_fault_Handler()__attribute__((weak ,alias("Default_Handler")));
+void MM_fault_Handler()__attribute__((weak ,alias("Default_Handler")));
+void Bus_fault_Handler()__attribute__((weak ,alias("Default_Handler")));
+void Usage_fault_Handler()__attribute__((weak ,alias("Default_Handler")));
 
-uint32_t vectors[] __attribute__((section(".vectors"))) = {
+
+
+uint32_t vectors[] __attribute__((section(".vectors")))= {
 	(uint32_t) &_stack_top,
 	(uint32_t) &Reset_Handler,
 	(uint32_t) &NMI_Handler,
-	(uint32_t) &H_Fault_Handler,
-	(uint32_t) &MM_Fault_Handler,
-	(uint32_t) &Bus_Fault_Handler,
-	(uint32_t) &Usage_Fault_Handler
+	(uint32_t) &H_fault_Handler,
+	(uint32_t) &MM_fault_Handler,
+	(uint32_t) &Bus_fault_Handler,
+	(uint32_t) &Usage_fault_Handler,
 };
 
 
-extern unsigned int _S_DATA;
-extern unsigned int _E_DATA;
-extern unsigned int _S_bss;
-extern unsigned int _E_bss;
-extern unsigned int _E_text;
-int i;
-
+extern uint32_t _S_DATA;
+extern uint32_t _E_DATA;
+extern uint32_t _S_bss;
+extern uint32_t _E_bss;
+extern uint32_t _E_text;
 
 void Reset_Handler()
 {
-	unsigned int DATA_SIZE = (unsigned char*)&_E_DATA - (unsigned char*)&_S_DATA;
-	unsigned char* P_src = (unsigned char*)&_E_text;
-	unsigned char* P_dst = (unsigned char*)&_S_DATA;
-	for( i=0;i<DATA_SIZE;i++)
+	//copy data from Flash to SRAM
+	uint32_t DATA_size = (unsigned char*)&_E_DATA - (unsigned char*) &_S_DATA;
+	//casting char to protect from memory not being aligned , even tho coping in int format is faster
+	unsigned char * P_src = (unsigned char*) &_E_text;
+	unsigned char * P_dst = (unsigned char*) &_S_DATA;
+	volatile int i;
+	for (i=0 ; i< DATA_size ; i++)
 	{
-		*((unsigned char*)P_dst++)=*((unsigned char*)P_src);
+		*((unsigned char *)P_dst++) = *((unsigned char*)P_src++);
 	}
-	unsigned int bss_SIZE = (unsigned char*)&_E_bss - (unsigned char*)&_S_bss;
-	P_dst=(unsigned char*)&_S_bss;
-	for( i=0;i<DATA_SIZE;i++)
+	uint32_t bss_size = (unsigned char*)&_E_bss -(unsigned char*)&_S_bss  ;
+	P_dst = (unsigned char*) &_S_bss;
+	
+	for (i=0 ; i< DATA_size ; i++)
 	{
-		*((unsigned char*)P_dst++)=(unsigned char*)0;
+		*((unsigned char *)P_dst++) = (unsigned char ) 0 ;
 	}
+	
 	main();
 }
