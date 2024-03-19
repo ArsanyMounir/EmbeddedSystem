da279d1a0060b0e22e2e1c0d53f06f4d7793c4c9
Arsany <Arsanyashrafmounir@gmail.com>
2024-03-19 15:29:07 +0200

    Lab1 RCC toggle led

--- Unit_6_MCU_Fundamentals/Lec_3/Lab1/main.c
+++ Unit_6_MCU_Fundamentals/Lec_3/Lab1/main.c
@@ -6,7 +6,7 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * <h2><center>&copy; Copyright (c) 2024 STMicroelectronics.
  * All rights reserved.</center></h2>
  *
  * This software component is licensed by ST under BSD 3-Clause license,
@@ -24,16 +24,21 @@
 #include"stdint.h"
 #include"stdio.h"
 #include"stdlib.h"
-#define RCC_BASE			0x40021000
+
+
 #define GPIOA_BASE			0x40010800
-#define RCC_APB2ENR			*(volatile uint32_t *)(RCC_BASE + 0x18)
 #define GPIOA_CRH			*(volatile uint32_t *)(GPIOA_BASE + 0x04)
 #define GPIOA_ODR			*(volatile uint32_t *)(GPIOA_BASE + 0x0C)
+#define RCC_BASE			0x40021000
+#define RCC_APB2ENR			*(volatile uint32_t *)(RCC_BASE + 0x18)
+
 
 int main(void)
 {
 	volatile int i;
+
 	RCC_APB2ENR |= (1<<2);
+
 	GPIOA_CRH   &= 0xFF0FFFFF;
 	GPIOA_CRH   |= 0x00200000;
 	while(1)
